2626eccb21d7f2286432c78b9df1a40e3dbf52c6
pawankona20 <119730775+pawankona20@users.noreply.github.com>
2022-12-16 13:59:07 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]==0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]==1)         {             printf("%d ",arr[i]);         }     } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==1)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+}
