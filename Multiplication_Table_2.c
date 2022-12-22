8a3bed16f257fb7497c90c44fef126d8dd55ea3a
pawankona20 <119730775+pawankona20@users.noreply.github.com>
2022-12-23 00:22:19 +0530

    #include<stdio.h> int main() {     int n,i,m,r;     scanf("%d%d%d",&n,&r);     for(i=1;i<=r;i++)     {
            printf("%d x %d = %d
    ",n,i,n*i);  
        }
        
        
    }

---
+++ Multiplication_Table_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,m,r;
+    scanf("%d%d%d",&n,&r);
+    for(i=1;i<=r;i++)
+    {
+        
+        printf("%d x %d = %d
+",n,i,n*i);  
+    }
+    
+    
+}
