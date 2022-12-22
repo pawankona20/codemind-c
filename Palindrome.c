187539cdd7d2f7e8ce98a53a42b61a61ab743c48
pawankona20 <119730775+pawankona20@users.noreply.github.com>
2022-12-22 18:56:17 +0530

    #include <stdio.h> int main() {   int n, reversed = 0, remainder, original;     scanf("%d", &n);     original = n;
        while (n != 0) 
        {
            remainder = n % 10;
            reversed = reversed * 10 + remainder;
            n /= 10;
        }
    
    
        if (original == reversed)
            printf("True");
        else
            printf("False");
    
        return 0;
    }

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+  int n, reversed = 0, remainder, original;
+    scanf("%d", &n);
+    original = n;
+
+    while (n != 0) 
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+
+    if (original == reversed)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
